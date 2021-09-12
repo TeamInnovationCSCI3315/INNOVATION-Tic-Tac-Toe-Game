53d26a604a7df36a89eee4e1b8d2e0898e79a6e5
Louis Mednick <louiemed@gmail.com>
2021-09-12 17:44:13 -0400

    Added CheckTurn()

--- INNOVATION-Tic-Tac-Toe-Game/BoardMethods.h
+++ INNOVATION-Tic-Tac-Toe-Game/BoardMethods.h
@@ -7,15 +7,11 @@ using namespace std;
 class BoardMethods
 {
 private:
-	int grid[9] = { 0,0,0,0,0,0,0,0,0 };
-	int turn;
+	string grid[9] = { "1","2","3","4","5","6","7","8","9" };
+	bool turn;
 public:
 	void DisplayGrid(int[]);
-
-
-
-
-
+	void CheckTurn(bool);
 
 
 };
