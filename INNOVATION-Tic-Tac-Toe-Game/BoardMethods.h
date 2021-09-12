7c42a982d3fec75c0826d94411cb72de1ee4e744
andrewmester1 <75629301+andrewmester1@users.noreply.github.com>
2021-09-12 17:22:46 -0400

    Added BoardMethods.h File

---
+++ INNOVATION-Tic-Tac-Toe-Game/BoardMethods.h
@@ -0,0 +1,22 @@
+#ifndef BOARDMETHODS_H
+#define BOARDMETHODS_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BoardMethods
+{
+private:
+	int grid[9] = { 0,0,0,0,0,0,0,0,0 };
+	int turn;
+public:
+	void DisplayGrid(int[]);
+
+
+
+
+
+
+
+};
+#endif
