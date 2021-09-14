90f3ec43e39d1ba81120173d31020018f800055f
andrewmester1 <75629301+andrewmester1@users.noreply.github.com>
2021-09-14 08:11:15 -0400

    FIxed Bug with Input Validation and blocking Player from inputting into another Player's space

--- INNOVATION-Tic-Tac-Toe-Game/BoardMethods.cpp
+++ INNOVATION-Tic-Tac-Toe-Game/BoardMethods.cpp
@@ -8,9 +8,9 @@
 using namespace std;
 bool BoardMethods::InputValid() //This boolean checks if the input the user will make is a valid input
 {
-	int PlayerChoiceTest = PlayerChoice+1;
+	int PlayerChoiceTest = PlayerChoice;
 	string validate = to_string(PlayerChoiceTest);
-		if(grid[PlayerChoice]!=validate)
+		if(grid[PlayerChoice-1]!=validate)
 		{
 			inputv=false;
 			return false;
@@ -29,31 +29,31 @@ void BoardMethods::PlayerInput()
 	{
 	switch(PlayerChoice) //Defined as a switch for every choice the player can make in the game 
 		{
-  	case 1:
-    	grid[0]="X";
+	case 1:
+		grid[0]="X";
 		break;
-  	case 2:
-    	grid[1]="X";
+	case 2:
+		grid[1]="X";
 		break;
-  	case 3:
+	case 3:
 		grid[2]="X";
 		break;
-  	case 4:
+	case 4:
 		grid[3]="X";
 		break;
-  	case 5:
+	case 5:
 		grid[4]="X";
 		break;
-  	case 6:
+	case 6:
 		grid[5]="X";
 		break;
-  	case 7:
+	case 7:
 		grid[6]="X";
 		break;
-  	case 8:
+	case 8:
 		grid[7]="X";
 		break;
-  	case 9:
+	case 9:
 		grid[8]="X";
 		break;
 		}
@@ -63,31 +63,31 @@ void BoardMethods::PlayerInput()
 {
 	switch(PlayerChoice) //Switch statement for all player choices in game
 		{
-  	case 1:
-    	grid[0]="O";
+	case 1:
+		grid[0]="O";
 		break;
-  	case 2:
-    	grid[1]="O";
+	case 2:
+		grid[1]="O";
 		break;
-  	case 3:
+	case 3:
 		grid[2]="O";
 		break;
-  	case 4:
+	case 4:
 		grid[3]="O";
 		break;
-  	case 5:
+	case 5:
 		grid[4]="O";
 		break;
-  	case 6:
+	case 6:
 		grid[5]="O";
 		break;
-  	case 7:
+	case 7:
 		grid[6]="O";
 		break;
-  	case 8:
+	case 8:
 		grid[7]="O";
 		break;
-  	case 9:
+	case 9:
 		grid[8]="O";
 		break;
 		}
@@ -109,7 +109,7 @@ void BoardMethods::CheckTurn()
 	{
 		cout << "Player 1's Turn: Enter a number (1-9) on the board\n"; //Player 1's Turn
 		cin >> PlayerChoice; // Takes in the input of the player
-		while(inputv==false)
+		while(!InputValid())
 		{
 			cout << "Space is Taken\n";
 			cin >> PlayerChoice;
@@ -117,13 +117,12 @@ void BoardMethods::CheckTurn()
 		PlayerInput();
 		DisplayGrid(); //Displays the choice just made
 		turn=true; //Ends the player's turn
-
 	}
-	else if (turn == true)
+	else
 	{
 		cout << "Player 2's Turn: Enter a number (1-9) on the board\n"; //Player 2's turn
 		cin >> PlayerChoice; //Takes in the input of the player
-		while(inputv==false)
+		while(!InputValid())
 		{
 			cout << "Space is Taken\n";
 			cin >> PlayerChoice;
@@ -137,7 +136,6 @@ void BoardMethods::CheckTurn()
 
 BoardMethods::BoardMethods()
 {
-	turn = false; //Sets the default turn
 	PlayerChoice = 0; //Sets the default choice for the player
 	inputv=true;
 }
